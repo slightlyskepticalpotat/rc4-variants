5352c5c8a5b6013ec2a55f747eedb7bd8d488ba4
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-11-03 14:05:43 -0400

    update readme to reflect RC4A support

---
+++ RC4.py
@@ -0,0 +1,93 @@
+class RC4:
+    """
+    Sample Python 3 implementation of RC4-drop[n] (MARK-4).
+    """
+
+    def __init__(self, key, n=0):
+        self.key = list(bytearray.fromhex(key))
+        self.s = self.KSA(self.key)
+        self.n = n
+
+    def KSA(self, key):
+        state = [i for i in range(256)]
+        j = 0
+        for i in range(256):
+            j = (j + state[i] + key[i % len(key)]) % 256
+            state[i], state[j] = state[j], state[i]
+        return state
+
+    def PRGA(self):
+        i = 0
+        j = 0
+        while True:
+            i = (i + 1) % 256
+            j = (j + self.s[i]) % 256
+            self.s[i], self.s[j] = self.s[j], self.s[i]
+            yield self.s[(self.s[i] + self.s[j]) % 256]
+
+    def convert_file(self, file):
+        with open(file, "r") as data:
+            return data.read()
+
+    def write_file(self, data):
+        with open("data", "w") as file:
+            file.write(data)
+
+    def cipher(self, plaintext, in_format="plain", out_format="plain"):
+        if in_format == "plain":
+            pass
+        elif in_format == "hex":
+            plaintext = bytearray.fromhex(plaintext).decode()
+        elif in_format == "file":
+            plaintext = self.convert_file(plaintext)
+        else:
+            raise NotImplementedError
+
+        plaintext = list(map(ord, plaintext))
+        keystream = self.PRGA()
+        for i in range(self.n):
+            next(keystream)
+        ciphertext = [plaintext[i] ^ next(keystream) for i in range(
+            len(plaintext))]
+
+        if out_format == "plain":
+            return "".join(map(chr, ciphertext))
+        elif out_format == "hex":
+            return bytes(ciphertext).hex()
+        elif out_format == "file":
+            self.write_file("".join(map(chr, ciphertext)))
+            return "data"
+        else:
+            raise NotImplementedError
+
+
+class RC4A(RC4):
+    """
+    Sample Python 3 implementation of RC4A.
+    """
+
+    def __init__(self, key1):
+        super().__init__(key1)
+        self.key1 = self.key
+        self.s1 = self.s
+        self.key2 = []
+
+        old_s = self.s1.copy()
+        for i in range(len(self.key1)):
+            self.key2.append(next(super().PRGA()))
+        self.s = old_s
+
+        self.s2 = self.KSA(self.key2)
+
+    def PRGA(self):
+        i = 0
+        j1 = 0
+        j2 = 0
+        while True:
+            i = (i + 1) % 256
+            j1 = (j1 + self.s1[i]) % 256
+            self.s1[i], self.s1[j1] = self.s1[j1], self.s1[i]
+            yield self.s2[(self.s1[i] + self.s1[j1]) % 256]
+            j2 = (j2 + self.s2[i]) % 256
+            self.s2[i], self.s2[j2] = self.s2[j2], self.s2[i]
+            yield self.s1[(self.s2[i] + self.s2[j2]) % 256]
--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# RC4-drop
-Sample Python 3 implementation of RC4-drop[n] (MARK-4), which discards the first n bytes of the keystream after key scheduling to improve security. An n in the range 768 <= n <= 3072 is recommended, but this implementation can also be used as regular RC4 with n = 0. Currently, supported forms of input and output include plain text, hexadecimal, and files.
+# RC4-variants
+Sample Python 3 implementation of various RC4 variants, including regular RC4 (ARC4), RC4A, and RC4-drop[n] (MARK-4). RC4A uses two state arrays and keys instead as one, whereas RC4-drop[n] discards the first n bytes of the keystream after key scheduling to improve security. A n in the range 768 <= n <= 3072 is recommended, while n = 0 corresponds to regular RC4.
 
-Since RC4 (including RC4-drop[n]) is widely believed to be insecure, using this implementation for non-educational purposes is not recommended. RC4 is a symmetric encryption algorithm, so you can decrypt the ciphertext by encrypting it again. Finally, there is also a helper function to help encrypt a file.
+Currently, supported forms of input and output include plain text, hexadecimal text (without an 0x), and files.
+
+Since RC4 (including RC4A and RC4-drop[n]) is widely believed to be insecure, using this implementation for non-educational purposes is not recommended. If you must, RC4-drop[n] with n = 3072 or RC4A should be more secure than RC4.
+
+All included variants of RC4 are symmetric encryption algorithms, so you can decrypt the ciphertext by encrypting it again.
 
 ## Usage
 ```py
-import RC4_drop
+import RC4
 
-rc4 = RC4_drop.RC4("0102030405", 768) # key, offset
+rc4 = RC4.RC4("0102030405", 768) # key, offset
 ciphertext_1 = rc4.cipher("0102030405", "hex", "plain") # hex plaintext, ascii ciphertext
 ciphertext_2 = rc4.cipher("foo", "plain", "file") # ascii plaintext, file ciphertext
 ciphertext_3 = rc4.cipher("bar", "file", "hex") # file plaintext, hex ciphertext
 print(ciphertext_1, ciphertext_2, ciphertext_3)
+rc4a = RC4.RC4A("0102030405") # key
+ciphertext_4 = rc4a.cipher("0102030405", "hex", "plain") # hex plaintext, ascii ciphertext
+ciphertext_5 = rc4a.cipher("foo", "plain", "file") # ascii plaintext, file ciphertext
+ciphertext_6 = rc4a.cipher("bar", "file", "hex") # file plaintext, hex ciphertext
+print(ciphertext_4, ciphertext_5, ciphertext_6)
 ```
